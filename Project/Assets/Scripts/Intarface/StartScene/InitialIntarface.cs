d06d99957c403d0fad7475af8e878ed3155fb6d5
Poruch <sura14081976@gmail.com>
2025-01-13 17:33:05 +0500

    add initial interface

---
+++ Project/Assets/Scripts/Intarface/StartScene/InitialIntarface.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class InitialIntarface : MonoBehaviour 
+{
+    public void StartGame()
+    {
+        //there should be a code here
+    }
+
+    public void OpenSettings() 
+    {
+        //there should be a code here
+    }
+
+    public void ExitGame()
+    {
+        //there should be a code here
+    }
+}
