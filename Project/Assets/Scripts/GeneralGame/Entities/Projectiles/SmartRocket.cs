c2763b7f9ff6b012172c9319f67cdea94cec0dcf
Poruch <sura14081976@gmail.com>
2025-04-16 13:56:30 +0500

    add smart rocket

---
+++ Project/Assets/Scripts/GeneralGame/Entities/Projectiles/SmartRocket.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.GeneralGame.Entities.Projectiles
+{
+    internal class SmartRocket : PhysicsBullet
+    {
+        [SerializeField]
+        GameObject Explosion;
+        protected override void AddFixedUpdate()
+        {
+            float angle = Time.deltaTime;
+            Dir = new Vector2(Dir.x * Mathf.Cos(angle) - Dir.y * Mathf.Sin(angle), Dir.x * Mathf.Sin(angle) + Dir.y * Mathf.Cos(angle));
+            transform.Rotate(0,0,angle * 57);
+            base.AddFixedUpdate();
+        }
+        protected override void OnCollide(GameObject otherGameObject)
+        {
+            base.OnCollide(otherGameObject);
+            Instantiate(Explosion,transform.position,Quaternion.identity);
+        }
+    }
+}
